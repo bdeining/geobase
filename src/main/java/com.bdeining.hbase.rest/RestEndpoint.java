2b24b61be1af206cb37838fbe4435ed9e3d17e49
bdeining <benjamin.deininger@connexta.com>
2019-12-09 16:14:38 -0700

    Added Blade

--- src/main/java/com.bdeining.hbase.rest/RestEndpoint.java
+++
@@ -1,4 +0,0 @@
-package com.bdeining.hbase.rest;
-
-public class RestEndpoint {
-}
---
+++ src/main/java/com/bdeining/hbase/rest/RestEndpoint.java
@@ -0,0 +1,10 @@
+package com.bdeining.hbase.rest;
+
+import com.blade.Blade;
+
+public class RestEndpoint {
+
+  public static void main(String[] args) {
+    Blade.of().get("/", ctx -> ctx.text("Hello Blade")).start();
+  }
+}
